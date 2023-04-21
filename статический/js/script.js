7749e1238d7e1b250f974361b97cf94313e8a598
Silicon01Valley <131396781+Silicon01Valley@users.noreply.github.com>
2023-04-21 16:28:49 +0300

    Update script.js

--- статический/js/script.js
+++ статический/js/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
 
