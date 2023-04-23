44fa154846fc153f584a143e1763286a63c88872
Silicon01Valley <131396781+Silicon01Valley@users.noreply.github.com>
2023-04-23 22:26:02 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let error = formValidate(form);
 
         let formData = new FormData(form);
+        formData.append('image', formImage.files[0]);
 
         if (error===0) {
             form.classList.add('_sending');
