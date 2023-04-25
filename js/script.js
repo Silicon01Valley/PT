d2867a4cb440904f26044e3c2d461e6110d35d40
Silicon01Valley <131396781+Silicon01Valley@users.noreply.github.com>
2023-04-25 10:47:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (error === 0) {
             form.classList.add('_sending');
-            let response = await fetch('js/sendmail.php', {
+            let response = await fetch('js/sendmail.php',{
                 method: 'POST',
                 body: formData
             });
